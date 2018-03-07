0c4977ac2655f52eaa755ac9fff50b001fe42695
Macostik <yura-granchenko@yandex.ru>
2018-03-07 10:04:19 +0200

    Initial Commit

---
+++ DynamicHeightStreamView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DynamicHeightStreamView
+//
+//  Created by Yura Granchenko on 3/7/18.
+//  Copyright © 2018 Yura Granchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
